91a615fe465a83242bf7c20ad2b9a3c31d225cd8
dGakamsky <46941271+dGakamsky@users.noreply.github.com>
2022-11-08 18:45:56 +0000

    implementing SC2 API

--- TSI css/index.js
+++ TSI css/index.js
@@ -2,6 +2,9 @@ let game1Div = document.getElementById("game1div")
 let game2Div = document.getElementById("game2div")
 let game3Div = document.getElementById("game3div")
 let audio = new Audio("01 Wings Of Liberty.mp3")
+let zergDiv = document.getElementById("zergGuide")
+let protossDiv = document.getElementById("protossGuide")
+let terranDiv = document.getElementById("terranGuide")
 
 function loadgame1(){game1Div.innerHTML += `<iframe width="300px" height="300px" src="https://www.addictinggames.com/embed/html5-games/20543" scrolling="yes" style="border:none"></iframe>`}
 
@@ -27,7 +30,31 @@ const options = {
 	}
 };
 
-fetch('https://starcraft-ii.p.rapidapi.com/learning/page/1/', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+function getZergGuide(){
+fetch('https://starcraft-ii.p.rapidapi.com/learning/zerg/page/1/', options)
+	.then(res => res.json())
+    .then(response=> {
+        zergDiv.innerHTML = `<a href= "${response.value[0].webUrl}"> Heres a helpful guide </a>  `
+        });
+
+}
+
+function getTerranGuide(){
+    fetch('https://starcraft-ii.p.rapidapi.com/learning/terran/page/1/', options)
+        .then(res => res.json())
+        .then(response=> {
+            terranDiv.innerHTML = `<a href= "${response.value[0].webUrl}"> Heres a helpful guide </a>  `
+            });
+    
+}
+
+function getProtossGuide(){
+fetch('https://starcraft-ii.p.rapidapi.com/learning/protoss/page/1/', options)
+	.then(res => res.json())
+    .then(response=> {
+        protossDiv.innerHTML = `<a href= "${response.value[0].webUrl}"> Heres a helpful guide </a>  `
+        });
+
+}
+    
+
